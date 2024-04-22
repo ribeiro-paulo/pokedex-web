eabe3effa8d9fc5cae6e5fc76409b2e18f43d54c
Paulo Victor <ribeiro.paulotm@gmail.com>
2024-04-22 09:16:46 -0400

    feat: new toolbar

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<template>
+    <v-toolbar color="yellow-darken-2">
+        <v-toolbar-title>
+            <v-icon>mdi-pokeball</v-icon>
+            Encontre seus pokemons favoritos
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
