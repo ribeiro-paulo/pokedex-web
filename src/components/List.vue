83cdd387d09f78e143820c51baba49003ce8ef01
Paulo Victor <ribeiro.paulotm@gmail.com>
2024-04-22 09:17:30 -0400

    feat: create infinite scroll

---
+++ src/components/List.vue
@@ -0,0 +1,56 @@
+<script>
+import Card from './Card.vue';
+import { getAllPokemons } from '../service/pokemonService';
+export default {
+    name: 'List',
+    props: {
+        listPaginated: Object
+    },
+    components: { Card },
+    data: () => ({
+        search: "",
+        nextUrl: "",
+        pokemons: []
+    }),
+    methods: {
+        select(pokemon) {
+            this.$emit('selectPokemon', pokemon)
+        },
+        async load({ done }) {
+            const { next, results } = await getAllPokemons(this.nextUrl || this.listPaginated.next)
+            this.nextUrl = next;
+            this.pokemons.push(...results);
+            done('ok');
+        },
+        compareSearch(str) {
+            const search = this.search.toLowerCase();
+            return str.toLowerCase().includes(search);
+        }
+    },
+    computed: {
+        onSearch() {
+            return this.pokemons.filter(item => {
+                const name = this.compareSearch(item.name);
+                const id = this.compareSearch(item.id.toString());
+                const types = this.compareSearch(item.types.join(', '));
+                const species = this.compareSearch(item.species);
+                return name || id || types || species;
+            });
+        }
+    },
+}
+</script>
+
+<template>
+    <v-main class="ma-2">
+        <v-text-field v-model.lazy.trim="search" label="Pesquisar" placeholder='Nome, id, tipo ou espécie' variant="outlined">
+        </v-text-field>
+        <v-infinite-scroll class="overflow-x-hidden" :items="onSearch" :onLoad="load">
+            <v-row class="pa-1">
+                <v-col v-for="pokemon in onSearch" :key="pokemon.id" class="d-flex justify-center">
+                    <Card :poke="pokemon" v-on:click="select(pokemon)" />
+                </v-col>
+            </v-row>
+        </v-infinite-scroll>
+    </v-main>
+</template>
